e12a23e6186f50a41b58a1c42a716bbef795d231
Rockman <gustavoandrade.net@gmail.com>
2015-08-18 16:12:14 -0300

    Fix title

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'Opencall',
+                'brandLabel' => 'Opencall - Open Source Helpdesk System',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
@@ -58,7 +58,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-left">&copy; Opencall - Open Source Helpdesk System <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
